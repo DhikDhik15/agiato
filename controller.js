1122274703787a26bf9883a15c4dfa849b13d5bc
DhikDhik15 <putraadhika15@gmail.com>
2020-11-30 22:57:06 +0700

    tampil data users

--- controller.js
+++ controller.js
@@ -1,8 +1,19 @@
 'use strict';
 
-var response = require ('./res');
+var response = require('./res');
 var connection = require('./koneksi');
 
 exports.index = function(req,res){
-    response.ok("Aplikasi REST API berjalan")
+    response.ok("Aplikasi REST API berjalan",res)
+};
+
+//menampilkan data
+exports.tampiluser = function(req, res){
+    connection.query('SELECT * FROM users', function(error, rows, fileds){
+        if(error){
+            connection.log(error);
+        }else{
+            response.ok(rows, res)
+        }
+    });
 };
--- routes.js
+++ routes.js
@@ -1,9 +1,12 @@
 'use strict';
 
 module.exports = function(app){
-    var jsonku = require('/.controller');
+    var jsonku = require('./controller');
 
     app.route('/')
         .get(jsonku.index);
+
+    app.route('/tampil')
+        .get(jsonku.tampiluser);
 
 }
